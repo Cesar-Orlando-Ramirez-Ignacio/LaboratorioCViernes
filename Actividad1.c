424b328dae969dbcd8df1076a652567a1b6e3be5
Cesar-Orlando-Ramirez-Ignacio <cr37746@gmail.com>
2019-08-24 17:35:17 -0500

    commit actualizado

--- Actividad1.c
+++ Actividad1.c
@@ -8,14 +8,14 @@
    Matricula: 1860455*/
 
 int main() {
-	float rf1,x, rf2;
-	printf("Introduzca un valor del 0 al 10 para x\n");
-	scanf("%f",&x);
-	rf1=(x-1)/4-(x-5)/36;
-	printf("El resultado de la funcion 1 es: %.2f\n",rf1);
-	
-	rf2=6*((x+1)/8-(2*x-3)/16);
-	printf("El resultado de la funcion 2 es: %.2f",rf2);
+	float rf1,x, rf2;                                      /* Se dan las variables que se utilizaran*/
+	printf("Introduzca un valor del 0 al 10 para x\n");    /*Pedimos al usuario introducir un valor del 0 al 10*/
+	scanf("%f",&x);                                        /* El usuario introduce el valor */
+	rf1=(x-1)/4-(x-5)/36;                                  /* Se ejecuta la operacion para darle el valor a la variable*/
+	printf("El resultado de la funcion 1 es: %.2f\n",rf1); /* Nos imprime el valor de la variable*/
+	 
+	rf2=6*((x+1)/8-(2*x-3)/16);                            /* Se ejecuta la operacion para darle el valor a la variable*/
+	printf("El resultado de la funcion 2 es: %.2f",rf2);   /* Nos imprime el valor de la variable*/
 	
 	
 	
--- actividad2.c
+++ actividad2.c
@@ -8,6 +8,15 @@
    Matricula: 1860455*/
  
 
-int main(int argc, char *argv[]) {
+int main() {
+	float x, rf1, rf2;                                      /* Se dan las variables que se utilizaran*/
+	printf("Ingresar el valor de x entre 0 y 10\n");        /*Pedimos al usuario introducir un valor del 0 al 10*/
+	scanf("%f",&x);                                         /* El usuario introduce el valor */
+	 
+	rf1=(2/3)*(x-(1-(x-2)/2))+1;                            /* Se ejecuta la operacion para darle el valor a la variable*/
+	rf2=2-(-2*(x+1)-(x-3)/2);                               /* Se ejecuta la operacion para darle el valor a la variable*/
+	
+	printf("El valor de la funcion 1 es: %.2f\n",rf1);      /* Nos imprime el valor de la variable*/
+	printf("El valor de la funcion 2 es: %.2f\n",rf2);      /* Nos imprime el valor de la variable*/
 	return 0;
 }
