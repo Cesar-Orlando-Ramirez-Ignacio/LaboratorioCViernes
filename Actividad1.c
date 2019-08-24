b3f8dc454a25c0659d5a098af66711547bf8787d
Cesar-Orlando-Ramirez-Ignacio <cr37746@gmail.com>
2019-08-24 00:53:40 -0500

    Primer commit Practical

--- Actividad1.c
+++ Actividad1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Autor: Cesar Orlando Ramirez Ignacio
+   fecha: 24/08/2019
+   grupo: 006
+   Horario: viernes 7am-9am
+   Matricula: 1860455*/
 
-int main(int argc, char *argv[]) {
+int main() {
+	float r,x;
+	printf("Introduzca un valor del 0 al 10 para x\n");
+	scanf("%f",&x);
+	r=(x-1)/4-(x-5)/36;
+	printf("El resultado es %.2f:",r);
 	return 0;
 }
